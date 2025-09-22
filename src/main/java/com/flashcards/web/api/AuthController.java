261e3b301abbba1c9bc8f5116bbe5dc2387a8120
piotrpojda <pitsubishi@gmail.com>
2025-09-22 10:56:55 +0200

    Moved login logic from Controller to service

--- src/main/java/com/flashcards/web/api/AuthController.java
+++ src/main/java/com/flashcards/web/api/AuthController.java
@@ -4,14 +4,8 @@ import com.flashcards.application.dto.LoginUserDto;
 import com.flashcards.application.dto.UserCreationDto;
 import com.flashcards.application.dto.UserDto;
 import com.flashcards.application.service.UserService;
-import com.flashcards.infrastructure.security.JwtService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
-    private final JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
 
-    public AuthController(UserService userService, JwtService jwtService, AuthenticationManager authenticationManager) {
+    public AuthController(UserService userService) {
         this.userService = userService;
-        this.jwtService = jwtService;
-        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("register")
@@ -38,17 +28,7 @@ public class AuthController {
 
     @PostMapping("login")
     public ResponseEntity<?> login(@RequestBody LoginUserDto request) {
-        Authentication authentication = authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(
-                request.identifier(),
-                request.password()
-            )
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-
-        String jwt = jwtService.generateToken(userDetails);
-        return ResponseEntity.ok(jwt);
+        return ResponseEntity.ok(userService.loginUser(request));
     }
 }
 
